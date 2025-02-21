e1c6ca90a21e0371134355fe1ad16411f7e46cc6
Avalondy <duyao2016@gmail.com>
2025-02-20 22:54:06 -0500

    Add logger

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::net::TcpListener;
 
+use env_logger::Env;
 use sqlx::PgPool;
 use zero2prod::{configuration, startup};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // Set up the logger, if the RUST_LOG environment variable is not set, default to info-level logging
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = configuration::get_configuration().expect("Failed to read configuration");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpServer};
+use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
 use sqlx::PgPool;
 
 use crate::routes;
@@ -10,6 +10,8 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            // Add the logger middleware
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
             // Get a pointer copy and attach it to the application state
