dc83654733136e7577c54bfc2172bcf3bcf99140
Avalondy <duyao2016@gmail.com>
2025-02-23 18:53:02 -0500

    Replace log with tracing for better observability

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, HttpResponse};
 use sqlx::{types::chrono::Utc, PgPool};
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -11,16 +12,24 @@ pub struct FormData {
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     // Generate a random unique identifier for this request
     let request_id = Uuid::new_v4();
-    log::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
+    // Using info_span! macro to create a new span and attach some values to its context
+    // This macro creates a span at the info-level
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        // % symbol means use their `Display` implementation for logging purposes
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
     );
-    log::info!(
-        "request_id {} - Saving new subscriber details in the database.",
-        request_id
-    );
+    // Use .enter() to activate the span, which returns a guard with type `Entered`
+    // As long as the guard is not dropped, all downstream spans and log events will be
+    // registed as children of the entered span
+    let _request_span_guard = request_span.enter();
+
+    // We do not call `.enter` on query_span!
+    // `.instrument` takes care of it at the right moments
+    // in the query future lifetime
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -32,21 +41,13 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         Utc::now(),
     )
     .execute(pool.get_ref())
+    // Attach the instrumentation then await it
+    .instrument(query_span)
     .await
     {
-        Ok(_) => {
-            log::info!(
-                "request_id {} - New subscriber details have been saved.",
-                request_id
-            );
-            HttpResponse::Ok().finish()
-        }
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
-            log::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
