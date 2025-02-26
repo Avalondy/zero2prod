3b21ef2562ffd00a700f09dd66e0e33e19029877
Avalondy <duyao2016@gmail.com>
2025-02-25 23:15:27 -0500

    Attach request_id to tracing logs

--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,22 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
+use actix_web::{
+    dev::Server,
+    web::{self, Data},
+    App, HttpServer,
+};
 use sqlx::PgPool;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // Wrap the pool in a smart pointer (Arc)
-    let db_pool = web::Data::new(db_pool);
+    let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
             // Add the logger middleware
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
             // Get a pointer copy and attach it to the application state
